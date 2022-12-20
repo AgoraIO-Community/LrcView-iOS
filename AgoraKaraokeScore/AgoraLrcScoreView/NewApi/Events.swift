74e1426647e628a01a26b814763d809b5819bcb0
ZYP <zhuyuping@agora.io>
2022-12-20 15:00:17 +0800

    更新注释

--- AgoraKaraokeScore/AgoraLrcScoreView/NewApi/Events.swift
+++ AgoraKaraokeScore/AgoraLrcScoreView/NewApi/Events.swift
@@ -30,18 +30,50 @@ import Foundation
 
 /// 分数计算协议
 @objc public protocol IScoreAlgorithm {
+    // MARK: - 自定义分数
+    
     /// 计算当前行(Line)的分数
     /// - Parameters:
     ///   - models: 字得分信息集合
     /// - Returns: 计算后的分数 [0, 100]
     @objc func getLineScore(with toneScores: [ToneScoreModel]) -> Int
     
+    // MARK: - 自定义激励分布
+    
+    /// 获取激励的数量
+    /// - Returns: 激励的总数量
     @objc func incentiveGradeCount() -> Int
+    
+    /// 计算当前分数属于哪种激励
+    /// - Parameter score: 一句的分数
+    /// - Returns: 激励的索引
     @objc func incentiveGradeIndex(score: Int) -> Int
+    
+    /// 激励的描述
+    /// - Note: 在视图中显示的名称
+    /// - Parameter gradeIndex: 激励索引
+    /// - Returns: 激励名称
     @objc func incentiveGradeDescription(gradeIndex: Int) -> String
+    
+    /// 激励是否可Combin
+    /// - Note: 如果可以，则在重复的时候追加显示 “X2” “X3”
+    /// - Parameter gradeIndex: 激励的索引
+    /// - Returns: Combinable
     @objc func incentiveCombinable(gradeIndex: Int) -> Bool
     
+    // MARK: - 自定义等级分布
+    
+    /// 获取等级分布的数量
     @objc func totalGradeCount() -> Int
+    
+    /// 计算当前分数属于哪个等级
+    /// - Parameter score: 当前句分数
+    /// - Returns: 等级索引
     @objc func totalGradeIndex(score: Int) -> Int
-    @objc func totalGradeDescription(gradeIndex: Int) -> Int
+    
+    /// 等级的描述
+    /// - Note: 在视图中显示的名称
+    /// - Parameter gradeIndex: 等级的索引
+    /// - Returns: 等级名称
+    @objc func totalGradeDescription(gradeIndex: Int) -> String
 }
