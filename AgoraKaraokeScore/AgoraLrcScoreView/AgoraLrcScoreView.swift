c19c965fed17e23e531f7cf5e7f96fdbe05a4c67
zhuyuping <954468540@qq.com>
2023-02-22 14:16:04 +0800

    Fix： CSD-54494
    fix: 54494
    feta: Update Log

--- AgoraKaraokeScore/AgoraLrcScoreView/AgoraLrcScoreView.swift
+++ AgoraKaraokeScore/AgoraLrcScoreView/AgoraLrcScoreView.swift
@@ -167,8 +167,10 @@ public class AgoraLrcScoreView: UIView {
     private var totalTime: TimeInterval = 0
     let logTag = "AgoraLrcScoreView"
     
-    public init(delegate: AgoraLrcViewDelegate) {
+    public init(delegate: AgoraLrcViewDelegate,
+                logDelegate: AgoraLogDelegate?) {
         super.init(frame: .zero)
+        Log.set(delegate: logDelegate)
         Log.info(text: "init == ", tag: logTag)
         setupUI()
         self.delegate = delegate
