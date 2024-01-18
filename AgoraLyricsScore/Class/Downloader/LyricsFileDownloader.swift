7042f9d2996dcce75e7c15e8c3c2621646b4fb7f
ZYP <954468540@qq.com>
2024-01-18 11:19:45 +0800

    fix cancel error

--- AgoraLyricsScore/Class/Downloader/LyricsFileDownloader.swift
+++ AgoraLyricsScore/Class/Downloader/LyricsFileDownloader.swift
@@ -93,6 +93,7 @@ public class LyricsFileDownloader: NSObject {
                 return
             }
             _cancleDownload(requestId: requestId)
+            _resumeTaskIfNeeded()
         }
     }
     
