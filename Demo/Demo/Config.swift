ba3e965ee5c6e332979a54fbcb03d8264a31b992
ZYP <954468540@qq.com>
2024-05-13 11:54:07 +0800

    update config

--- Demo/Demo/Config.swift
+++ Demo/Demo/Config.swift
@@ -8,12 +8,21 @@
 import Foundation
 
 struct Config {
+    /// ignore
     static let channelId = "DRMTest001"
     static let hostUid: UInt = 1
     static let audioUid: UInt = 2
     static let playerUid: Int = 100
     static let mccUid: Int = 333
+    
+    /// agora important vars
     static let rtcAppId = <#rtcAppId without Token#>
     static let mccAppId = <#mccAppId#>
     static let mccCertificate = <#mccCertificate#>
+    
+    /// ysd important vars
+    static let pid = <#pid#>
+    static let pKey = <#pKey#>
+    static let token = <#token#>
+    static let userId = <#userId#>
 }
