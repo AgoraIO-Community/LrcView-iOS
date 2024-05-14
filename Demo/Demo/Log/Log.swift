3a6142c7c5d64a89ad4a5d3e33427dce047bd7da
ZYP <954468540@qq.com>
2024-05-14 11:40:35 +0800

    demo: feat: update demo log

--- Demo/Demo/Log/Log.swift
+++ Demo/Demo/Log/Log.swift
@@ -9,6 +9,9 @@ import Foundation
 import AgoraComponetLog
 
 class Log {
+    static let shared = Log()
+    var componetLog: AgoraComponetLog!
+    
     static func setupLogger() {
         let fileLogger = AgoraComponetFileLogger(logFilePath: nil,
                                                  filePrefixName: "AgoraLyricsDemo",
@@ -16,31 +19,32 @@ class Log {
                                                  maxFileCount: 5,
                                                  domainName: "ALD")
         let consoleLogger = AgoraComponetConsoleLogger(domainName: "ALD")
-        AgoraComponetLog.setLoggers([fileLogger, consoleLogger])
+        shared.componetLog = AgoraComponetLog(queueTag: "AgoraLyricsDemo")
+        shared.componetLog.configLoggers([fileLogger, consoleLogger])
     }
     
     static func errorText(text: String,
                           tag: String? = nil) {
-        AgoraComponetLog.error(withText: text, tag: tag)
+        shared.componetLog.error(withText: text, tag: tag)
     }
     
     static func error(error: CustomStringConvertible,
                       tag: String? = nil) {
-        AgoraComponetLog.error(withText: error.description, tag: tag)
+        shared.componetLog.error(withText: error.description, tag: tag)
     }
     
     static func info(text: String,
                      tag: String? = nil) {
-        AgoraComponetLog.info(withText: text, tag: tag)
+        shared.componetLog.info(withText: text, tag: tag)
     }
     
     static func debug(text: String,
                       tag: String? = nil) {
-        AgoraComponetLog.debug(withText: text, tag: tag)
+        shared.componetLog.debug(withText: text, tag: tag)
     }
     
     static func warning(text: String,
                         tag: String? = nil) {
-        AgoraComponetLog.warning(withText: text, tag: tag)
+        shared.componetLog.warning(withText: text, tag: tag)
     }
 }
