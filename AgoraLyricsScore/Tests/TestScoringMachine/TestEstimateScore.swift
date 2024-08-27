4f86a7e813a51d4605c863e25575a63910d8b912
ZYP <954468540@qq.com>
2024-08-27 16:01:25 +0800

    update unit test

--- AgoraLyricsScore/Tests/TestScoringMachine/TestEstimateScore.swift
+++ AgoraLyricsScore/Tests/TestScoringMachine/TestEstimateScore.swift
@@ -13,6 +13,13 @@ class TestEstimateScore: XCTestCase, ScoringMachineDelegate {
     var vm: ScoringMachine!
     var fileName = ""
     let expBad1_A = XCTestExpectation(description: "Bad1_A")
+    let expGood1_A = XCTestExpectation(description: "Good1_A")
+    let expGood2_A = XCTestExpectation(description: "Good2_A")
+    let expGood3_A = XCTestExpectation(description: "Good3_A")
+    let expGood4_A = XCTestExpectation(description: "Good4_A")
+    let exp_Bad1_B = XCTestExpectation(description: "exp_Bad1_B")
+    let exp_Good1_B = XCTestExpectation(description: "Good1_B")
+    let exp_Good2_B = XCTestExpectation(description: "exp_Good1_B")
     
     override func setUpWithError() throws {
 //        Log.setLoggers(loggers: [ConsoleLogger()])
@@ -27,36 +34,43 @@ class TestEstimateScore: XCTestCase, ScoringMachineDelegate {
     func testGood1_A() { /// cumulativeScore:1726
         fileName = "qilixiang_good1_50ms_pitch"
         goTest(fileName: fileName, xmlFileName: "660250")
+        wait(for: [expGood1_A], timeout: 3)
     }
     
     func testGood2_A() { /// cumulativeScore:626
         fileName = "qilixiang_good2_50ms_pitch"
         goTest(fileName: fileName, xmlFileName: "660250")
+        wait(for: [expGood2_A], timeout: 3)
     }
     
     func testGood3_A() { /// cumulativeScore:2283
         fileName = "qilixiang_good3_50ms_pitch"
         goTest(fileName: fileName, xmlFileName: "660250")
+        wait(for: [expGood3_A], timeout: 3)
     }
     
     func testGood4_A() { /// cumulativeScore:2110
         fileName = "qilixiang_good4_50ms_pitch"
         goTest(fileName: fileName, xmlFileName: "660250")
+        wait(for: [expGood4_A], timeout: 3)
     }
     
     func testBad1_B() { /// cumulativeScore:2953
         fileName = "houlai_bad1_50ms_pitch"
         goTest(fileName: fileName, xmlFileName: "900318")
+        wait(for: [exp_Bad1_B], timeout: 3)
     }
     
     func testGood1_B() { /// cumulativeScore:4454
         fileName = "houlai_good1_50ms_pitch"
         goTest(fileName: fileName, xmlFileName: "900318")
+        wait(for: [exp_Good1_B], timeout: 3)
     }
     
     func testGood2_B() { /// cumulativeScore:4535
         fileName = "houlai_good2_50ms_pitch"
         goTest(fileName: fileName, xmlFileName: "900318")
+        wait(for: [exp_Good2_B], timeout: 3)
     }
     
     func goTest(fileName: String, xmlFileName: String) {
@@ -127,6 +141,30 @@ class TestEstimateScore: XCTestCase, ScoringMachineDelegate {
             if cumulativeScore == 1943 {
                 expBad1_A.fulfill()
             }
+            if cumulativeScore == 1726 {
+                expGood1_A.fulfill()
+            }
+            if cumulativeScore == 626 {
+                expGood2_A.fulfill()
+            }
+            if cumulativeScore == 2283 {
+                expGood3_A.fulfill()
+            }
+            if cumulativeScore == 2110 {
+                expGood4_A.fulfill()
+            }
+            
+            
+            if cumulativeScore == 2953 {
+                exp_Bad1_B.fulfill()
+            }
+            if cumulativeScore == 4454 {
+                exp_Good1_B.fulfill()
+            }
+            if cumulativeScore == 4535 {
+                exp_Good2_B.fulfill()
+            }
+            
             print("cumulativeScore:\(cumulativeScore)")
             print("---------\(fileName)  end ----------\n")
         }
